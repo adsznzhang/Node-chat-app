602d917c29ce7434b4c465a1ae7c6a5d8a5e2521
adsznzhang <adszn@hotmail.com>
2017-10-24 21:50:37 +0800

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,17 +3,12 @@ var socket = io();
 socket.on('connect', () => {
     console.log('Connected to the server');
 
-
-    socket.emit('createMessage', {
-        from: 'dadfa@hotmail.com',
-        text: 'waowao'
-    });
 });
 
 socket.on('disconnect', () => {
     console.log('Disconnected from server');
 });
 
-socket.on('newMessageEvent', (newMessage) => {
+socket.on('newMessage', (newMessage) => {
     console.log('New Message', newMessage);
 });
